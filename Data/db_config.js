6d54826a8a871ae34fc0b159071203488d572853
Nathan Broaddus <nathanbroaddus@gmail.com>
2018-04-17 14:32:33 -0400

    [link0shrink] editing db cred

--- Data/db_config.js
+++ Data/db_config.js
@@ -1,8 +1,10 @@
 const dbConfig = {
-  user: 'node_user',
-  host: 'localhost',
-  database: 'linksdb',
-  port: 5432
+  // user: 'node_user',
+  // host: 'localhost',
+  // database: 'linksdb',
+  // port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 }
 
 module.exports = dbConfig
